505376ca1bb7564bc66cd200d6ac34c835ac38f0
Pedro Sland <pedro_sland@hotmail.com>
2016-01-03 13:13:11 +0000

    Improved version of anagram checker
    Handles spaces and counts occurrences correctly

--- anagrams/main.go
+++ anagrams/main.go
@@ -11,12 +11,23 @@ func main() {
 }
 
 func anagram(s1, s2 string) bool {
+	// discard spaces
+	s1 = strings.Replace(s1, " ", "", -1)
+	s2 = strings.Replace(s2, " ", "", -1)
+
 	if len(s1) != len(s2) {
 		return false
 	}
 
+	s1Chars := make(map[rune]int)
+
 	for _, r := range s1 {
-		if !strings.ContainsRune(s2, r) {
+		// works because empty type is 0
+		s1Chars[r]++
+	}
+
+	for r, count := range s1Chars {
+		if strings.Count(s2, string(r)) != count {
 			return false
 		}
 	}
